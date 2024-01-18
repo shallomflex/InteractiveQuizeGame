e1e67c9a5312d17f4c17579dcb5b95ca377da511
nenshallom <sshallom543@gmail.com>
2024-01-18 14:05:02 +0100

    4th commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: "which is the largest animal in the world?",
+        answer: [
+            {text: "shark", correct:false},
+            {text: "blue whale", correct:true},
+            {text: "Elephant", correct:false},
+            {text: "Giraffe", correct:false},
+        ]
+    },
+    {
+        question: "which is the smallest city in the world?",
+        answer: [
+            {text: "Vatican city", correct:true},
+            {text: "bhutan", correct:false},
+            {text: "Nepal", correct:false},
+            {text: "shri lanka", correct:false},
+        ]
+    },
+    {
+        question: "which is the largest desert in the world?",
+        answer: [
+            {text: "kalahari", correct:false},
+            {text: "gobi", correct:false},
+            {text: "sahara", correct:false},
+            {text: "Antartica", correct:true},
+        ]
+    },
+    {
+        question: "which is the smallest continent in the world?",
+        answer: [
+            {text: "Asia", correct:false},
+            {text: "Austrelia", correct:true},
+            {text: "Arctic", correct:false},
+            {text: "Africa", correct:false},
+        ]
+    },
+
+]
